d4dc22e50b54fd88d2f375d382b651459d18e749
Jochen van Wylick <jowylick@microsoft.com>
2020-01-23 14:46:52 +0100

    Working app - @localhost/greeting

---
+++ src/app/jvwapp/src/test/java/com/example/jvwapp/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jvwapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
