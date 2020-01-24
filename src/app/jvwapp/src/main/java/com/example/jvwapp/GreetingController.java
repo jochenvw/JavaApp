f9bcf64f97d4141e24fd69c8c83859d5e801ba6f
Jochen van Wylick <jowylick@microsoft.com>
2020-01-24 13:43:38 +0100

    Added Exception in greeting controller to see results in App Insights

--- src/app/jvwapp/src/main/java/com/example/jvwapp/GreetingController.java
+++ src/app/jvwapp/src/main/java/com/example/jvwapp/GreetingController.java
@@ -13,7 +13,10 @@ public class GreetingController {
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) throws Exception {
+		if(name.equals("johndoe")) {
+			throw new Exception("Sure ....");
+		}
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
