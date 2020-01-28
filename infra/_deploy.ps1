3a5c8fd1c8d1bcdedc2bb411f50693c3eda11944
Jochen van Wylick <jowylick@microsoft.com>
2020-01-28 17:32:26 +0100

    Added application dashboard as ARM template

--- infra/_deploy.ps1
+++ infra/_deploy.ps1
@@ -1,1 +1,3 @@
-New-AzResourceGroupDeployment -ResourceGroupName ms-csu-nl-jvw-javademoapp-test -TemplateFile .\application-resources.json -TemplateParameterFile .\parameters.test.json -Debug
+New-AzResourceGroup -Name ms-csu-nl-jvw-javademoapp-test -Location westeurope
+New-AzResourceGroupDeployment -ResourceGroupName ms-csu-nl-jvw-javademoapp-test -TemplateFile .\application-resources.json -TemplateParameterFile .\parameters.test.json -Verbose
+New-AzResourceGroupDeployment -ResourceGroupName ms-csu-nl-jvw-javademoapp-test -TemplateFile .\dashboard.json -TemplateParameterFile .\parameters.test.json -Verbose
