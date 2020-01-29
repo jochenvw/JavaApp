a51fd744a30b0bcee3718b6869857891d021dcc5
Jochen van Wylick <jowylick@microsoft.com>
2020-01-29 21:08:47 +0100

    Added KeyVault secret resolution with MSI auth

--- src/app/jvwapp/src/main/java/com/example/jvwapp/GreetingController.java
+++ src/app/jvwapp/src/main/java/com/example/jvwapp/GreetingController.java
@@ -2,9 +2,15 @@ package com.example.jvwapp;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.microsoft.azure.AzureEnvironment;
+import com.microsoft.azure.credentials.AppServiceMSICredentials;
+import com.microsoft.azure.keyvault.KeyVaultClient;
+import com.microsoft.azure.keyvault.models.SecretBundle;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class GreetingController {
@@ -14,9 +20,20 @@ public class GreetingController {
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) throws Exception {
-		if(name.equals("johndoe")) {
+		if (name.equals("johndoe")) {
 			throw new Exception("Sure ....");
 		}
+
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@GetMapping("/secret")
+	public Greeting secret(@RequestParam(value = "name", defaultValue = "sample") String name) throws Exception {
+		String keyVaultEndpoint = System.getenv("KEYVAULTURL");
+		AppServiceMSICredentials credentials = new AppServiceMSICredentials(AzureEnvironment.AZURE);
+		KeyVaultClient keyVaultClient = new KeyVaultClient(credentials);
+		SecretBundle bundle = keyVaultClient.getSecret(keyVaultEndpoint,name);
+
+		return new Greeting(counter.incrementAndGet(), String.format("Vault secret value is %s!", bundle.value()));
 	}
 }
