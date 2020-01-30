a44bfeb78e3bba5d1844697c67791eab574e4a76
Jochen van Wylick <jowylick@microsoft.com>
2020-01-30 11:49:00 +0100

    Added telemetry tracking for app insights for KeyVault response times

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Repository with Spring Boot application - with pipeline as code, infra-as-code (
 
 
 [![Build Status](https://dev.azure.com/jvwdemo/JavaApp/_apis/build/status/jochenvw.JavaApp?branchName=master)](https://dev.azure.com/jvwdemo/JavaApp/_build/latest?definitionId=47&branchName=master)
+
--- src/app/jvwapp/src/main/java/com/example/jvwapp/GreetingController.java
+++ src/app/jvwapp/src/main/java/com/example/jvwapp/GreetingController.java
@@ -1,19 +1,28 @@
 package com.example.jvwapp;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.microsoft.applicationinsights.TelemetryClient;
 import com.microsoft.azure.AzureEnvironment;
 import com.microsoft.azure.credentials.AppServiceMSICredentials;
 import com.microsoft.azure.keyvault.KeyVaultClient;
 import com.microsoft.azure.keyvault.models.SecretBundle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
+
+
 @RestController
 public class GreetingController {
+
+	@Autowired
+	TelemetryClient telemetryClient;
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
@@ -29,10 +38,22 @@ public class GreetingController {
 
 	@GetMapping("/secret")
 	public Greeting secret(@RequestParam(value = "name", defaultValue = "sample") String name) throws Exception {
+		Instant start = Instant.now();
+
+		// Environment variable is injected from AppSettings
 		String keyVaultEndpoint = System.getenv("KEYVAULTURL");
+		// MSI used for auth to the KeyVault - which has an access policy set
+		// for the identity of the web app
 		AppServiceMSICredentials credentials = new AppServiceMSICredentials(AzureEnvironment.AZURE);
 		KeyVaultClient keyVaultClient = new KeyVaultClient(credentials);
 		SecretBundle bundle = keyVaultClient.getSecret(keyVaultEndpoint,name);
+
+		// Track dependency call with application insights - which should
+		// show up in the application map
+		Instant finish = Instant.now();
+		int timeElapsed = (int) Duration.between(start, finish).toMillis();
+		telemetryClient.trackDependency("KeyVault", "GetSecret",
+		new com.microsoft.applicationinsights.telemetry.Duration(0, 0, 0, 0, timeElapsed), true);
 
 		return new Greeting(counter.incrementAndGet(), String.format("Vault secret value is %s!", bundle.value()));
 	}
