9b2ca5016d9c96135983bd832ff207f9e542f459
Jochen van Wylick <jowylick@microsoft.com>
2020-02-06 17:35:08 +0100

    Resolving KeyVaul secrets now through App Service

--- src/app/jvwapp/src/main/java/com/example/jvwapp/GreetingController.java
+++ src/app/jvwapp/src/main/java/com/example/jvwapp/GreetingController.java
@@ -6,21 +6,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.time.Duration;
-import java.time.Instant;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 import com.microsoft.applicationinsights.TelemetryClient;
-import com.microsoft.azure.AzureEnvironment;
-import com.microsoft.azure.credentials.AppServiceMSICredentials;
-import com.microsoft.azure.keyvault.KeyVaultClient;
-import com.microsoft.azure.keyvault.models.SecretBundle;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +28,9 @@ public class GreetingController {
 	@Value("${jvw.file}")
 	private String filename;
 
+	@Value("${jvw.keyvaultsecret}")
+	private String keyVaultSecret;
+
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
@@ -49,24 +44,7 @@ public class GreetingController {
 
 	@GetMapping("/secret")
 	public Greeting secret(@RequestParam(value = "name", defaultValue = "sample") String name) throws Exception {
-		Instant start = Instant.now();
-
-		// Environment variable is injected from AppSettings
-		String keyVaultEndpoint = System.getenv("KEYVAULTURL");
-		// MSI used for auth to the KeyVault - which has an access policy set
-		// for the identity of the web app
-		AppServiceMSICredentials credentials = new AppServiceMSICredentials(AzureEnvironment.AZURE);
-		KeyVaultClient keyVaultClient = new KeyVaultClient(credentials);
-		SecretBundle bundle = keyVaultClient.getSecret(keyVaultEndpoint, name);
-
-		// Track dependency call with application insights - which should
-		// show up in the application map
-		Instant finish = Instant.now();
-		int timeElapsed = (int) Duration.between(start, finish).toMillis();
-		telemetryClient.trackDependency("KeyVault", "GetSecret",
-				new com.microsoft.applicationinsights.telemetry.Duration(0, 0, 0, 0, timeElapsed), true);
-
-		return new Greeting(counter.incrementAndGet(), String.format("Vault secret value is %s!", bundle.value()));
+		return new Greeting(counter.incrementAndGet(), String.format("Vault secret value is %s!", keyVaultSecret));
 	}
 
 	@GetMapping("/file")
