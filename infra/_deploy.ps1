76fae789b2c99c75af2914d9d78ae2fd8a993ddc
Jochen van Wylick <jowylick@microsoft.com>
2020-03-10 17:13:50 +0100

    Minor code reformatting

--- infra/_deploy.ps1
+++ infra/_deploy.ps1
@@ -1,3 +1,16 @@
-New-AzResourceGroup -Name ms-csu-nl-jvw-javademoapp-test -Location westeurope
-New-AzResourceGroupDeployment -ResourceGroupName ms-csu-nl-jvw-javademoapp-test -TemplateFile .\application-resources.json -TemplateParameterFile .\parameters.test.json -Verbose
-New-AzResourceGroupDeployment -ResourceGroupName ms-csu-nl-jvw-javademoapp-test -TemplateFile .\dashboard.json -TemplateParameterFile .\parameters.test.json -Verbose
+New-AzResourceGroup `
+    -Name ms-csu-nl-jvw-javademoapp-test `
+    -Location westeurope
+
+
+New-AzResourceGroupDeployment `
+    -ResourceGroupName ms-csu-nl-jvw-javademoapp-test `
+    -TemplateFile .\application-resources.json `
+    -TemplateParameterFile .\parameters.test.json `
+    -Verbose
+
+New-AzResourceGroupDeployment `
+    -ResourceGroupName ms-csu-nl-jvw-javademoapp-test `
+    -TemplateFile .\dashboard.json `
+    -TemplateParameterFile .\parameters.test.json `
+    -Verbose
