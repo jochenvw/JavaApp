4b46303c28c92819694238a60f88be6b8d4d035e
Jochen van Wylick <jowylick@microsoft.com>
2020-03-10 20:41:27 +0100

    Moved property file value checks to /info controller

--- src/app/jvwapp/src/main/java/com/example/jvwapp/DemoApplication.java
+++ src/app/jvwapp/src/main/java/com/example/jvwapp/DemoApplication.java
@@ -1,27 +1,11 @@
 package com.example.jvwapp;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
-	@Value("${azure.keyvault.uri}")
-	private static String keyVaultUri;
-
-	@Value("${azure.application-insights.instrumentation-key}")
-	private static String appInsightsKey;
-
-	@Value("${my.special.secret}")
-	private static String kvSecret;
-
 	public static void main(String[] args) {
-		System.out.println("App started - with follwing config:");
-		System.out.println("KV URI" + keyVaultUri);
-		System.out.println("App insights key" + appInsightsKey);
-		System.out.println("KeyVaultSecret" + kvSecret);
-
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
--- src/app/jvwapp/src/main/java/com/example/jvwapp/InfoController.java
+++ src/app/jvwapp/src/main/java/com/example/jvwapp/InfoController.java
@@ -1,13 +1,28 @@
 package com.example.jvwapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InfoController {
+	@Value("${azure.keyvault.uri}")
+	private String keyVaultUri;
+
+	@Value("${azure.application-insights.instrumentation-key}")
+	private String appInsightsKey;
+
+	@Value("${my.special.secret}")
+	private String kvSecret;
+
 	@GetMapping("/info")
 	public ResponseEntity greeting() throws Exception {
-		return ResponseEntity.ok("All good here!");
+		String response = "";
+		response += "App started - with follwing config:" + "\r\n";
+		response += "KV URI" + keyVaultUri + "\r\n";
+		response += "App insights key" + appInsightsKey + "\r\n";
+		response += "KeyVaultSecret" + kvSecret + "\r\n";
+		return ResponseEntity.ok(response);
 	}
 }
