005d8c5643b198dcca52590cf24c0d3c4cd5bb9e
Jochen van Wylick <jowylick@microsoft.com>
2020-03-10 22:49:26 +0100

    Re-enabled keyvault secret resolving

--- src/app/jvwapp/src/main/java/com/example/jvwapp/InfoController.java
+++ src/app/jvwapp/src/main/java/com/example/jvwapp/InfoController.java
@@ -13,8 +13,8 @@ public class InfoController {
 	@Value("${azure.application-insights.instrumentation-key}")
 	private String appInsightsKey;
 
-	//@Value("${my.special.secret}")
-	//private String kvSecret;
+	@Value("${my.special.secret}")
+	private String kvSecret;
 
 	@GetMapping("/info")
 	public ResponseEntity greeting() throws Exception {
